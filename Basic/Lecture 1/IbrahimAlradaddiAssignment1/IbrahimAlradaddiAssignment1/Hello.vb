c5d8d8374f8f5f3a59a9a0e32637108331c38ca6
M.Zubair <szubair1833@gmail.com>
2018-09-09 01:37:00 +0500

    Second commit

---
+++ Basic/Lecture 1/IbrahimAlradaddiAssignment1/IbrahimAlradaddiAssignment1/Hello.vb
@@ -0,0 +1,27 @@
+﻿Module Hello
+
+
+    Sub Main()
+        System.Console.WriteLine("Hello World")
+
+        System.Console.Write("Title: ")
+        System.Console.WriteLine(My.Application.Info.Title)
+        System.Console.Write("Description: ")
+        System.Console.WriteLine(My.Application.Info.Description)
+        System.Console.Write("Company Name: ")
+        System.Console.WriteLine(My.Application.Info.CompanyName)
+        System.Console.Write("Product Name: ")
+        System.Console.WriteLine(My.Application.Info.ProductName)
+        System.Console.Write("Copyright: ")
+        System.Console.WriteLine(My.Application.Info.Copyright)
+        System.Console.Write("Assembly Version: ")
+        System.Console.WriteLine(My.Application.Info.Version)
+
+
+        System.Console.WriteLine("Please press [Enter] to close.")
+        System.Console.ReadLine()
+
+    End Sub
+
+
+End Module
---
+++ Basic/Lecture 1/IbrahimAlradaddiAssignment1/IbrahimAlradaddiAssignment1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("IbrahimAlradaddiAssignment1")>
+<Assembly: AssemblyDescription("In this project we will learn how to use read and write command and how to use my object")>
+<Assembly: AssemblyCompany("Basil Ibrahim Alraddadi")>
+<Assembly: AssemblyProduct("IbrahimAlradaddiAssignment1")>
+<Assembly: AssemblyCopyright("Basil Ibrahim Alraddadi")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e731d061-1479-4d2b-bf86-88ffb4af2df7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.1")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
